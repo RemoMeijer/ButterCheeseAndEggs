e6d28288a9dd965def1d10417ebcf43299848a75
RemoMeijer <54670814+RemoMeijer@users.noreply.github.com>
2020-10-13 14:13:50 +0200

    Add files via upload
    Init

---
+++ ButterCheeseAndEggs/ButterCheeseAndEggs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ButterCheeseAndEggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
